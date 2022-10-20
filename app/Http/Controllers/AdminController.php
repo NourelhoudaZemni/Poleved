231304147306a894104f8026b0adaf53bdde83b3
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-10-20 21:54:04 +0100

    Add admin controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+
+use App\Models\Balade;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+ public function view_category(){ 
+   $data = category::all();
+return view('admin.category', compact('data'));
+
+ }
+ 
+ public function view_balade(){ 
+
+   $category = category::all();
+
+return view('admin.balade', compact('category'));
+
+ }
+ public function add_category(Request $request){
+    $data = new category;
+    $data -> category_name =$request -> category;
+    $data -> save();
+    return redirect()->back()-> with('message','Category added successfully'); 
+     }
+
+     public function ajouter_balade(Request $request){
+     $balade = new balade;
+     $balade -> title = $request -> title;
+     $balade -> description = $request -> description;
+     $balade -> price = $request -> price;
+     $balade -> quantity = $request -> quantity;
+     $balade -> discount_price = $request -> discount_price;
+     $balade -> category = $request -> category; 
+     $balade -> save();
+
+      return redirect()->back()->with('message', 'balade ajoutée avec succés');
+       }
+
+
+     public function delete_category($id){
+      $data=category::find($id);
+      $data->delete();
+      return view('admin.balade');
+     }
+
+     public function delete_balade($id){
+      $balade=balade::find($id);
+      $balade->delete();
+      return redirect()->back();
+     }
+
+     public function show_balade(){
+$balade = balade::all();
+      return view('admin.show_balade', compact ('balade'));
+     }
+}
