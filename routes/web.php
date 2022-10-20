5e015b3b0b67ceefeddfe7f147b8825db2ec9214
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-10-20 21:55:12 +0100

    add routing to views

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BaladeController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,5 +27,23 @@ Route::view('/profile', 'profile');
 Route::view('/contact', 'contact');
 Route::view('/notFound404', 'notFound404');
 
+route::get('/view_category', [AdminController::class, 'view_category']);
+
+ 
+
+route::post('/add_category', [AdminController::class, 'add_category']);
+
+route::get('/delete_category/{id}', [AdminController::class, 'delete_category']);
+
+route::get('/delete_balade/{id}', [AdminController::class, 'delete_balade']);
+
+Route::resource("/balade", BaladeController::class);
+
+route::get('/view_balade', [AdminController::class, 'view_balade']);
+
+
+route::post('/ajouter_balade', [AdminController::class, 'ajouter_balade']); 
+
+route::get('/show_balade', [AdminController::class, 'show_balade']);
 
 
