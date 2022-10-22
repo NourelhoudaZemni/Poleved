a7c26867766f1682ab658d6fba012b5e4856cb41
Wael <wael.bouatay@esprit.tn>
2022-10-22 03:44:04 +0100

    fix table velo !

--- database/migrations/2022_10_18_000529_create_velos_table.php
+++ database/migrations/2022_10_18_000529_create_velos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('velo', function (Blueprint $table) {
+        Schema::create('velos', function (Blueprint $table) {
             $table->id();
             $table->string('marque');
             $table->string('etat');
