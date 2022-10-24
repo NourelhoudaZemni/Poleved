17337ff04c826ee123a5213a91e958a9946b2c84
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-10-25 00:12:24 +0200

    add validators add balade

--- app/Http/Controllers/BaladeClientController.php
+++ app/Http/Controllers/BaladeClientController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-
+use Illuminate\Support\Facades\Validator;
 use App\Models\Balade;
 use Illuminate\Http\Request;
 
@@ -18,16 +18,61 @@ class BaladeClientController extends Controller
      
 
 
-    public function create()
+    public function create(Request $req)
     {
-        return view('baladesclient.create');
+
+
+        $category = category::all();
+     
+        $balade = Balade::all();
+
+        return view('baladesclient.create', compact('category'));
     }
   
     public function store(Request $request)
     {
-        $input = $request->all();
-        Balade::create($input);
-        return redirect('balade')->with('flash_message', 'balades Addedd!');  
+        
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required', 
+            'price'=>'required', 
+            'discount_price'=>'required',
+            'category'=>'required', 
+            'quantity'=>'required', 
+            'image'=>'required',             'date'=>'required|after:today',
+
+        
+        ],
+            
+    ['title.required'=>'Entrez votre le titre  s il vous plait',
+    'date.after'=>'Start date must be after Today',
+
+    'description.required'=>'Entrez votre la description  s il vous plait',
+    'price.required'=>'Entrez le prix  s il vous plait',
+    'discount_price.required'=>'Entrez votre le prix après reduction  s il vous plait',
+    'category.required'=>'Entrez la catégorie s il vous plait',
+    'quantity.required'=>'Entrez la quantité s il vous plait',
+    'date.required'=>'Entrez la date s il vous plait',
+
+   ]);
+
+
+        $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+
+        request()->image->move(public_path('images'), $file_name);
+        $balade = new Balade;
+
+        $balade->title= $request->title;
+        $balade->description= $request->description;
+        $balade->price= $request->price;
+        $balade->discount_price= $request->discount_price;
+        $balade->category= $request->category;
+        $balade->quantity= $request->quantity; 
+        $balade->date= $request->date; 
+        $balade->image = $file_name;
+        $balade->save();
+
+        return redirect()->route('balade.create')->with('success', 'Added successfully.');
     }
     
     public function show($id)
